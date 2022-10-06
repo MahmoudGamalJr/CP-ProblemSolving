4ec12ed91b0013a0ac71aabcc8fcc0db898c3170
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-06 13:09:21 +0200

    Arrays and Strings

---
+++ HackerRank/1D Arrays in C .cpp
@@ -0,0 +1,28 @@
+// Problem Link: https://www.hackerrank.com/challenges/1d-arrays-in-c/problem
+// Langugae: C
+// User: mahmoud_gamal_a3
+#include<stdio.h>
+
+int main()
+{
+
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        int SumOfArray = 0;
+        for(int i = 0; i < n; i++)
+        {
+            SumOfArray = SumOfArray + arr[i];
+        }
+
+        printf("%d\n", SumOfArray);
+
+
+
+
+       return 0;
+}
