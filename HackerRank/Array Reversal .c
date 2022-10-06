45f0699df1c2e7b1d688a3a1f7bc6bb8a445f2cc
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-06 13:16:30 +0200

    Arrays and Strings

---
+++ HackerRank/Array Reversal .c
@@ -0,0 +1,28 @@
+// Problem Link: https://www.hackerrank.com/challenges/reverse-array-c/problem
+// Langugae: C
+// User: mahmoud_gamal_a3
+
+#include<stdio.h>
+
+int main()
+{
+
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for(int i = 0; i < n ; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        for(int i = n - 1; i >= 0 ; i--)
+        {
+            printf("%d ", arr[i]);
+        }
+
+        
+
+
+
+
+       return 0;
+}
