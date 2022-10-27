c4d1ddc6705ebc6c510d9f3907c749ce2d86e5a0
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-27 16:26:10 +0200

    Create Backpack Packing.cpp

---
+++ CS Academy/Backpack Packing.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    
+    vector<int> arr(n);
+    for(int i = 0; i < n; ++i)
+        cin >> arr[i];
+    
+    int cnt = n;
+    for(int i = 0; i < n; ++i) {
+        if(a >= b && a >= arr[i]) {
+            --cnt;
+            a -= arr[i];
+        } else if(b >= a && b >= arr[i]) {
+            --cnt;
+            b -= arr[i];
+        }
+    }
+    
+    cout << cnt << endl;
+    
+    return 0;
+}
